976001d7268321dc629878ba8c353803e2758a94
Khylle <khylle.lim@obf.ateneo.edu>
2022-04-18 20:59:10 +0800

    Update urls.py
    Edited links

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -5,11 +5,11 @@ from .views import *
 
 
 urlpatterns = [
-    path('', login, name='login'),
+    path('login', login, name='login'),
 
     path('signup', signup, name='signup'),
 
     path('signup_save', signup_save, name ='signup_save'),
 
-    path('home/', home, name='home')
+    path('', home, name='home')
 ] 

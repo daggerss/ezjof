c5afd99980f6e4aa5b90f7aed70450ec3de5e545
Dagny Eran Yenko <dagny.yenko@obf.ateneo.edu>
2022-04-19 21:08:46 +0800

    Implement sort for joffeedapp
    - Sort current JOFs by status
    - Sort everything else by date

--- joffeedapp/views.py
+++ joffeedapp/views.py
@@ -5,25 +5,24 @@ from django.contrib.auth.decorators import login_required
 
 @login_required    
 def jofarchive(request):
-    jofs = JOF.objects.all()
+    jofs = JOF.objects.all().order_by('-date')
     return render(request, 'joffeed/archivedJOFs.html', {"jofs":jofs})
 
 @login_required
 def jofcurrent(request):
-    jofs = JOF.objects.all()
+    jofs = JOF.objects.all().order_by('status')
     return render(request, 'joffeed/currentJOFs.html', {"jofs":jofs})
 
 @login_required
 def jofrush(request):
-    jofs = JOF.objects.all()
+    jofs = JOF.objects.all().order_by('date')
     return render(request, 'joffeed/rushJOFs.html', {"jofs":jofs})
 
 @login_required
 def jofsettings(request):
-    jofs = JOF.objects.all()
-    return render(request, 'joffeed/settingsJOF.html', {"jofs":jofs})
+    return render(request, 'joffeed/settingsJOF.html')
 
 @login_required
 def joffeed(request):
-    jofs = JOF.objects.all()
+    jofs = JOF.objects.all().order_by('date')
     return render(request, 'joffeed/JOFfeed.html', {"jofs":jofs})
